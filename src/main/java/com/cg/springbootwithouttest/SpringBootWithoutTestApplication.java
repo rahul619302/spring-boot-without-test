ad7b2467e1b3b2a788f11d58a73e18c41b610be8
rahul619302 <rahul.ab.singh@capgemini.com>
2018-11-20 10:38:38 +0530

    initial commit

---
+++ src/main/java/com/cg/springbootwithouttest/SpringBootWithoutTestApplication.java
@@ -0,0 +1,12 @@
+package com.cg.springbootwithouttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithoutTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithoutTestApplication.class, args);
+	}
+}
